1a7a9ea9b34f2392c08899046171c8f6233401e2
KANG JIHYO <luxuryjihyo@naver.com>
2024-07-29 17:33:49 +0900

    first commit

---
+++ src/main/java/com/example/messageplatform/MessageplatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.messageplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageplatformApplication.class, args);
+    }
+
+}
