c8966e284f8c3226a67fc5be1e2da43f06fef405
KANG JIHYO <luxuryjihyo@naver.com>
2024-08-05 12:45:47 +0900

    Feat : profile categories convert to flat list

--- src/main/java/com/example/messageplatform/nhn/kakao/service/NhnKakaoService.java
+++ src/main/java/com/example/messageplatform/nhn/kakao/service/NhnKakaoService.java
@@ -16,10 +16,8 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class NhnKakaoService {
@@ -34,11 +32,42 @@ public class NhnKakaoService {
         try{
             String appKey = configuration.getAppKey();
 
-            Map<String,Object> result = nhnKakoaClient.getCategories(appKey);
-            return result;
+            Map<String,Object> response = nhnKakoaClient.getCategories(appKey);
+            List<Map<String,Object>> categories =(List<Map<String,Object>>) response.get("categories");
+            List<Map<String,Object>> result = convertToFlatList(categories);
+            response.put("categories",result);
+            return response;
         }catch (Exception e){
             throw new RuntimeException(e);
         }
+    }
+
+    private List<Map<String,Object>> convertToFlatList(List<Map<String,Object>> items){
+        List<Map<String, Object>> flatList = new ArrayList<>();
+        items.forEach(item -> pathBasedEncoding(item, new StringBuilder(), new StringBuilder(), flatList));
+
+        return flatList;
+    }
+
+    private Map<String,Object> pathBasedEncoding(Map<String,Object> map,StringBuilder parentName, StringBuilder parentPath, List<Map<String, Object>> flatList) {
+        String code = (String)map.get("code");
+        String name = (String)map.get("name");
+
+        StringBuilder currentPath = new StringBuilder(parentPath).append(code);
+        StringBuilder currentName = parentName.length() > 0 ? new StringBuilder(parentName).append(",").append(name) : new StringBuilder(name);
+
+        Map<String,Object> result = new HashMap<>();
+        result.put("code",currentPath.toString());
+        result.put("name",currentName.toString());
+
+        List<Map<String,Object>> subList = (List<Map<String,Object>>) map.get("subCategories");
+        if(subList != null){
+            subList.forEach(item -> pathBasedEncoding(item, currentName,currentPath, flatList));
+        } else {
+            flatList.add(result);
+        }
+
+        return result;
     }
 
 }
